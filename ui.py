67509e1580fa93f5ef9710e76916a70562e03e1e
David Chong <sumbrella2001@yahoo.com.sg>
2019-03-09 23:10:42 +0000

    Integrated HMM count predictor and opencv videostream with eye_aspect_ratio detection algorithm. Awaiting data file from mune

--- ui.py
+++ ui.py
@@ -1,11 +1,103 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pywt
+from hmmlearn import hmm
 import pygame, time, sys, os
 from random import randint
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import imutils
+import time
+import dlib
+import cv2
 
-def blinkrate():
-    
-    return randint(-10,10)
+class BlinkDetector:
+    def __init__(self, shape_predictor_file, batch_interval):
+        self.vs = VideoStream(src=0).start()
+        self.EAR = []
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(shape_predictor_file)
+        (self.lStart, self.lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (self.rStart, self.rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+        self.batchInterval = batch_interval
+        self.timeElapsed = 0
+        self.prevTime = -1
+        self.blinkR = -1
 
+    def eye_aspect_ratio(self, eye):
+        # compute the euclidean distances between the two sets of
+        # vertical eye landmarks (x, y)-coordinates
+        A = dist.euclidean(eye[1], eye[5])
+        B = dist.euclidean(eye[2], eye[4])
 
+        # compute the euclidean distance between the horizontal
+        # eye landmark (x, y)-coordinates
+        C = dist.euclidean(eye[0], eye[3])
+
+        # compute the eye aspect ratio
+        ear = (A + B) / (2.0 * C)
+
+        # return the eye aspect ratio
+        return ear
+
+    def blinkrate(self):
+        model = hmm.GaussianHMM(n_components=2)
+        model.fit(np.array(self.EAR).reshape(-1,1))
+        states = model.predict(np.array(self.EAR).reshape(-1,1))
+        count = 0
+        prevstate = 1
+        for i in range(0,len(states)):
+            if states[i] == 0 and prevstate == 1:
+                starts.append(i)
+                count += 1
+                prevstate = 0
+            elif states[i] == 1 and prevstate == 0:
+                prevstate = 1
+            elif states[i] == 1 and prevstate == 0:
+                ends.append(i)
+        self.EAR.clear()
+        return count
+
+    def processFrame(self):
+        if self.prevTime == -1:
+            self.prevTime = time.time()
+        elif self.timeElapsed < self.batchInterval:
+            newTime = time.time()
+            self.timeElapsed += newTime - self.prevTime
+            self.prevTime = newTime
+        else:
+            self.timeElapsed = 0
+            self.blinkR = self.blinkrate()
+            self.prevTime = time.time()
+
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rects = self.detector(gray, 0)
+        for rect in rects:
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = self.predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            # extract the left and right eye coordinates, then use the
+            # coordinates to compute the eye aspect ratio for both eyes
+            leftEye = shape[self.lStart:self.lEnd]
+            rightEye = shape[self.rStart:self.rEnd]
+            leftEAR = self.eye_aspect_ratio(leftEye)
+            rightEAR = self.eye_aspect_ratio(rightEye)
+
+            # average the eye aspect ratio together for both eyes
+            ear = (leftEAR + rightEAR) / 2.0
+
+            # save datapoint
+            self.EAR.append(ear)
+        return self.blinkR
+ 
 class Background:
     def __init__(self, imageFilePath, width, height):
         self.background = pygame.transform.scale(pygame.image.load(imageFilePath),(width,height))
@@ -30,25 +122,22 @@ charwidth = 100
 charheight = 50
 averageBlinkR = 0
 def main():
+    # Initialise video stream
+    bd = BlinkDetector("File_To_Shape_Detector")
     not_done = True
     pygame.init()
     screen = pygame.display.set_mode((900,300), pygame.RESIZABLE)
     bg = Background("background.jpeg", 900,300)
     character1 = pygame.transform.scale(pygame.image.load("rocket.png"), (charwidth,charheight))
     character = pygame.transform.rotate(character1, 1)
-
-    
-    #get the blink rate 
-    #BLINKKKK
     
     try:
         counter = 1
         charY = bg.height/2
-        blinkR = averageBlinkR
         while not_done:
+            blinkR = bd.processFrame()
             if((counter % 10) == 0):
                 print("updating y")
-                blinkR += blinkrate()/100
                 charY = bg.height/2 + (bg.height/2)*blinkR
                 if((abs(charY - bg.height) < charheight/2)):
                     charY = bg.height - charheight/2
